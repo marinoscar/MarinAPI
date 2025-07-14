ae5e40d22b69d63c9a7c4b7e554a6d02dcdb19af
Oscar Marin <oscar@marin.cr>
2025-07-14 18:35:31 -0500

    Add XML parser for economic indicators
    Introduces the Parser class to convert XML data into a list of IndicatorRecord objects, extracting code, value, date, and mapping names. This enables structured handling of economic indicator data from XML sources.

---
+++ src/BCCR.TipoDeCambio/Parser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BCCR.TipoDeCambio
+{
+    public class Parser
+    {
+        public record IndicatorRecord(int Code, string type,  string? Name, double Value, DateTime Date);
+
+        public List<IndicatorRecord> ToList(string xml, string type)
+        {
+            var nsDiffgr = XNamespace.Get("urn:schemas-microsoft-com:xml-diffgram-v1");
+            var document = XDocument.Parse(xml);
+
+            var records = document.Descendants("INGC011_CAT_INDICADORECONOMIC")
+                .Select(el =>
+                {
+                    try
+                    {
+                        int code = int.Parse(el.Element("COD_INDICADORINTERNO")?.Value ?? "0");
+                        double value = double.Parse(el.Element("NUM_VALOR")?.Value ?? "0", CultureInfo.InvariantCulture);
+                        DateTime date = DateTime.Parse(el.Element("DES_FECHA")?.Value ?? DateTime.Now.ToString());
+
+                        return new IndicatorRecord(code, type, Mapping.GetName(code),  value, date);
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                })
+                .Where(record => record != null)
+                .ToList();
+
+            return records!;
+        }
+    }
+}
