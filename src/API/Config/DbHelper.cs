d49327c25b22e7d11f58573a098f03628e4b689c
Oscar Marin <oscar@marin.cr>
2025-07-14 13:03:58 -0500

    Create DbHelper.cs

---
+++ src/API/Config/DbHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Config
+{
+    public class DbHelper
+    {
+        public static string GetConnectionString()
+        {
+            var dbServer = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost";
+            var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "postgres";
+            var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "password";
+            var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "logging";
+            return $"Host={dbServer};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword}";
+        }
+    }
+}
